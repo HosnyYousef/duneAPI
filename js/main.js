7cbff9715e9aaa44527633cefa5961248de505b4
Yousef Hosny <Yousefhosny.hm@gmail.com>
2023-11-09 17:04:02 +0200

    dune API was not working, so I switched it to chuck Norris API

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
-fetch("https://the-dune-api.herokuapp.com/quotes")
+fetch("https://api.chucknorris.io/jokes/random")
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data[0])
       document.querySelector('h2').innerText = data[0].id
-      document.querySelector('h3').innerText = data[0].quote
+      document.querySelector('h3').innerText = data.value.quote
     })
     .catch(err => {
         console.log(`error ${err}`)
